374876afda4d5b6755704380af76729035374c98
AyushR97j <ayushraj803110@gmail.com>
2024-02-06 23:57:23 +0530

    Error Page Changed

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -8,11 +8,11 @@ function Error() {
   return (
     <div className=" flex flex-col justify-center items-center flex-1  text-white text-3xl font-medium bg-bg-richblack-800">
 
-    <div className="flex flex-col justify-center items-center gap-6 bg-[#082f49] shadow w-[450px] h-[200px] rounded-sm">
+    <div className="flex flex-col justify-center items-center gap-6 bg-[#082f49] shadow w-[450px] h-[200px] rounded-xl">
       <div className=" flex items-center justify-center rounded-lg">
         Error 404 - Page Not Found
       </div>
-      <button className="bg-[#115e59] py-2 px-4 rounded-full flex gap-4 shadow"
+      <button className="bg-[#115e59] py-2 px-4 rounded-full flex gap-4 shadow hover:bg-[#40827e] duration-1000"
         onClick = {() => navigate(-1)}>
           <FaArrowLeft
             className=" my-1"
